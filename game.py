67f448ee8454c4affcd7086787af1b4839265b5f
Dmitry Bykov <bykov.dmitry86@yandex.ru>
2025-07-08 16:28:44 +0300

    Initial commit

---
+++ game.py
@@ -0,0 +1,18 @@
+
+class configuration():
+    def __init__(self):
+        self.read() 
+    
+    def read(self):
+        pass
+
+    def chenge(self):
+        pass
+
+def next_move():
+    pass
+
+
+
+if __name__ == "__main__":
+    print('Начало игры')
